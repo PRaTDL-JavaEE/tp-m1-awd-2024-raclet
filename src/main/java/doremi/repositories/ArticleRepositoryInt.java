31d38015690c55f9dc112b8b808181f271206b3c
raclet <raclet@irit.fr>
2024-01-28 21:14:45 +0100

    Resolved #4a : findAllArticle de ArticleService

---
+++ src/main/java/doremi/repositories/ArticleRepositoryInt.java
@@ -0,0 +1,10 @@
+package doremi.repositories;
+
+import doremi.domain.Article;
+import java.util.List;
+
+public interface ArticleRepositoryInt {
+
+    public List<Article> findAllArticles();
+
+}
---
+++ src/main/java/doremi/services/ArticleService.java
@@ -0,0 +1,27 @@
+package doremi.services;
+
+import doremi.domain.Article;
+import doremi.repositories.ArticleRepositoryInt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    @Autowired
+    private ArticleRepositoryInt articleRepository;
+
+    public List<Article> findAllArticles() {
+        return articleRepository.findAllArticles();
+    }
+
+    public ArticleRepositoryInt getArticleRepository() {
+        return articleRepository;
+    }
+
+    public void setArticleRepository(ArticleRepositoryInt articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+}
