af97e3172b5394ee1e3ddefa481d949e2f6ad834
raclet <raclet@irit.fr>
2024-01-28 21:19:19 +0100

    Resolved #4b : mise en place de ArticleController

---
+++ src/main/java/doremi/controllers/ArticleController.java
@@ -0,0 +1,29 @@
+package doremi.controllers;
+
+import doremi.services.ArticleService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("/articles")
+    public String list(Model model){
+        model.addAttribute("articles", articleService.findAllArticles());
+        return "articles";
+    }
+
+    public ArticleService getArticleService() {
+        return articleService;
+    }
+
+    public void setArticleService(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+}
