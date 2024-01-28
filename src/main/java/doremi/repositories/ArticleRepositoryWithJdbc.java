866bf2eb418ed66906f31a110267f5a9a9a648ad
raclet <raclet@irit.fr>
2024-01-28 21:24:50 +0100

    Resolved #4c : findArticleById de ArticleService

--- src/main/java/doremi/repositories/ArticleRepositoryWithJdbc.java
+++ src/main/java/doremi/repositories/ArticleRepositoryWithJdbc.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,6 +45,42 @@ public class ArticleRepositoryWithJdbc implements ArticleRepositoryInt {
         }
 
         return articles;
+    }
+
+    public Article findArticleById(int id) {
+        Article article = null;
+        ResultSet fetchedArticle;
+        Connection connexion = null;
+
+        try {
+            connexion = dataSource.getConnection();
+            PreparedStatement statement =
+                    connexion.prepareStatement("SELECT id, title, category FROM articles WHERE id = ?");
+
+            statement.setInt(1, id);
+            fetchedArticle = statement.executeQuery();
+
+            if(fetchedArticle.next()) {
+                article = new Article();
+                article.setArticleId(fetchedArticle.getInt("id"));
+                article.setTitle(fetchedArticle.getString("title"));
+                article.setCategory(fetchedArticle.getString("category"));
+            }
+            statement.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connexion != null)
+                    connexion.close();
+            } catch(SQLException sqle) {
+                sqle.printStackTrace();
+            }
+        }
+
+        return article;
+
     }
 
 }
